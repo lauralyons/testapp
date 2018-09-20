78dcc7e286033f43b2d969dc220471b44141a856
laura lyons <lauraslyons@gmail.com>
2018-09-20 14:52:38 -0600

    trying to update price in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_225007) do
+ActiveRecord::Schema.define(version: 2018_09_20_200354) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
