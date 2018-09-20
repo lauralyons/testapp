d74872c3cd3e598766284a8b7c7ad0116d5751e2
laura lyons <lauraslyons@gmail.com>
2018-09-20 17:01:21 -0600

    remove decimal column

---
+++ db/migrate/20180920225302_remove_decimal_from_products.rb
@@ -0,0 +1,5 @@
+class RemoveDecimalFromProducts < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :products, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_200354) do
+ActiveRecord::Schema.define(version: 2018_09_20_225302) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
