d74aa1898c8190394d54f9063b223fdf16abb551
laura lyons <lauraslyons@gmail.com>
2018-09-21 14:37:03 -0600

    adding order index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'orders/index'
+  get 'orders/show'
   root 'simple_pages#landing_page'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
