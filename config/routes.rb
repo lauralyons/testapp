ebd0e06ea138595890bc1ed60ac6ac030bd8d36d
laura lyons <lauraslyons@gmail.com>
2018-09-24 13:37:36 -0600

    removed reduntant routes to show and index in orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'orders/index'
-  get 'orders/show'
   root 'simple_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
