79d00eb0d270477fc3019b0a20b19dae880a8859
laura lyons <lauraslyons@gmail.com>
2018-10-14 20:09:30 -0600

    added seach bar, added column size on products, added bootstrap to form in edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_190521) do
+ActiveRecord::Schema.define(version: 2018_10_14_213922) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_190521) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.decimal "price"
+    t.string "size"
   end
 
 end
