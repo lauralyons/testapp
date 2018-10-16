2ea7b06314253b1afe5380e3acc831a9d6bfb4b0
laura lyons <lauraslyons@gmail.com>
2018-10-16 15:04:41 -0600

    update user_mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: "lauraslyons@gmail.com"
-
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  def contact_form(@email, @name, @message)
+  @message = message
+    mail(from: email,
+         to: 'lauraslyons@gmail.com',
+         subject: "A new contact form message from #{name}")
   end
 end
