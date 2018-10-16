c0c9f9b8f9618e8f35511fb7dd6cb3e134a959c3
laura lyons <lauraslyons@gmail.com>
2018-10-16 15:51:13 -0600

    changed parameters user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: "lauraslyons@gmail.com"
-  
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  def contact_form(@email, @name, @message)
-  @message = message
-    mail(from: email,
-         to: 'lauraslyons@gmail.com',
-         subject: "A new contact form message from #{name}")
-  end
+
+  def contact_form(email, name, message)
+		@message = message
+		mail(from: email, to: "lauraslyons@gmail.com", subject: "Rails Web App message from #{name}")
+	end
 end
