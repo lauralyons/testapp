9e7a098589f9caee24cec3479ac54f89d7ead61a
laura lyons <lauraslyons@gmail.com>
2018-11-20 15:35:11 -0700

    added welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,11 @@ class UserMailer < ApplicationMailer
 		@message = message
 		mail(from: email, to: "lauraslyons@gmail.com", subject: "Rails Web App message from #{name}")
 	end
+
+  def welcome(user)
+    @appname = "Belle Waters Jewelry"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
+
 end
