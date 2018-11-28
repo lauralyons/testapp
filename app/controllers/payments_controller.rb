d7d6f11bb93caa98970de1050b2bb3635d319a31
laura lyons <lauraslyons@gmail.com>
2018-11-28 16:08:58 -0700

    6.6 added payment controller

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,30 @@
+class PaymentsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price * 100).round(2).to_i,
+        currency: "usd",
+        source: token,
+        description: params[:stripeEmail],
+        receipt_email: @user.email
+      )
+      if charge.paid
+        Order.create(
+          product_id: @product.id,
+          user_id: @user.id,
+          total: @product.price
+        )
+      end
+    rescue Stripe::CardError => e
+      # The card has been declined
+    end
+    redirect_to product_path(@product)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'payments/create'
   devise_for :users, controllers: { registrations: "user_registrations" }
   resources :products do
     resources :comments
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
+  post 'payments/create'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
