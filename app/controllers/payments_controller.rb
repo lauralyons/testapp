7971f06c6ab51a1d053088ab845ae3564dc55e66
laura lyons <lauraslyons@gmail.com>
2018-12-17 19:08:51 -0700

    stripe error messages

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -21,10 +21,15 @@ class PaymentsController < ApplicationController
           user_id: @user.id,
           total: @product.price
         )
+        flash[:success] = "Your payment was processed successfully"
       end
+      
     rescue Stripe::CardError => e
       # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-    redirect_to product_path(@product)
+    redirect_to product_path(@product), notice: "Thank you for your purchase."
   end
 end
